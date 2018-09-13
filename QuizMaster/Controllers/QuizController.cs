f3fd97a4a6989512396144b969f1581d6425dee9
wim van laer <wim.van_laer@hotmail.com>
2018-09-13 20:14:49 +0200

    added routes to QuizController

--- QuizMaster/Controllers/QuizController.cs
+++ QuizMaster/Controllers/QuizController.cs
@@ -1,14 +1,43 @@
+using System.Threading.Tasks;
 using API.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using QuizMaster.Controllers.Base;
 using QuizMaster.Shared.Models;
 
 namespace QuizMaster.Controllers
 {
+    [Produces("application/json")]
+    [Route("api/v1/[controller]")]
     public class QuizController : CRUDControllerBase<Quiz>
     {
         public QuizController(IDataService<Quiz> dataService)
             : base(dataService)
         {
         }
+
+        [HttpPost]
+        [Route("api/v1/[controller]/")]
+        public override async Task<ActionResult> CreateAsync([FromBody] Quiz quiz)
+            => await base.CreateAsync(quiz);
+
+        [HttpGet]
+        [Route("api/v1/[controller]/")]
+        public override async Task<ActionResult> GetAllAsync()
+            => await base.GetAllAsync();
+
+        [HttpGet]
+        [Route("api/v1/[controller]/{id}")]
+        public override async Task<ActionResult> GetOneAsync([FromQuery] int id)
+            => await base.GetOneAsync(id);
+
+        [HttpPut]
+        [Route("api/v1/[controller]/{id}")]
+        public override async Task<ActionResult> UpdateAsync([FromBody] Quiz quiz, [FromQuery] int id)
+            => await base.UpdateAsync(quiz, id);
+
+        [HttpDelete]
+        [Route("api/v1/[controller]/{id}")]
+        public override async Task<ActionResult> DeleteAsync(int id)
+            => await base.DeleteAsync(id);
     }
 }
