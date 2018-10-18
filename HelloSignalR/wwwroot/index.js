e9413aab472dbf95474b7d1d4149fea7cd722269
wim van laer <van.laer.wim@gmail.com>
2018-10-18 00:40:32 -0700

    Created seperate file for js

---
+++ HelloSignalR/wwwroot/index.js
@@ -0,0 +1,25 @@
+(async function () {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("/app")
+        .build();
+
+    const app = new Vue({
+        el: "#app",
+        data: {
+            messages: [],
+            newMessage: null
+        },
+        methods: {
+            async addMessage() {
+        await connection.invoke("Send", this.newMessage);
+        this.newMessage = null;
+    }
+}
+});
+
+    connection.on("Send", message => {
+        app.messages.push(message);
+});
+
+    await connection.start();
+})();
