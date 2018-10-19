3e6baab48603d2b3c86970328fdc9f2e7ef01459
Wim Van Laer <van.laer.wim@gmail.com>
2018-10-19 20:57:43 +0200

    Created SocketApp.Services

---
+++ QuizMaster.SocketApp.Services/Interfaces/IDataService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using QuizMaster.Shared.Models.Base;
+
+namespace QuizMaster.API.Services.Interfaces
+{
+    public interface IDataService<T> where T : IModel
+    {
+        Task<T> CreateAsync(T model);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetOneAsync(int id);
+        Task<TProperty> GetPropertyAsync<TProperty>(int id, Func<T, TProperty> selector);
+        Task<T> UpdateAsync(T model, int id);
+        Task<bool> DeleteAsync(int id);
+    }
+}
