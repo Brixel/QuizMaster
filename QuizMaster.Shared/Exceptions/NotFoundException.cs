0bbae80dc3584f8c6332cbd7b474b5a3eaf345b1
Wim Van Laer <van.laer.wim@gmail.com>
2018-10-19 22:51:05 +0200

    Gave NotFoundException a type paramter for identification

--- QuizMaster.Shared/Exceptions/NotFoundException.cs
+++ QuizMaster.Shared/Exceptions/NotFoundException.cs
@@ -1,9 +1,12 @@
 using System;
+using QuizMaster.Shared.Models.Base;
 
 namespace QuizMaster.Shared.Exceptions
 {
-    public class NotFoundException : Exception
+    public class NotFoundException<T> : Exception where T : Model
     {
+        public int IdNotFound { get; set; } = -1;
+        
         public NotFoundException()
         {
         }
@@ -11,6 +14,13 @@ namespace QuizMaster.Shared.Exceptions
         public NotFoundException(string message)
             : base(message)
         {
+        }
+
+        
+        public NotFoundException(int idNotFound, string message)
+            : base(message)
+        {
+            IdNotFound = idNotFound;
         }
 
         public NotFoundException(string message, Exception innerException)
