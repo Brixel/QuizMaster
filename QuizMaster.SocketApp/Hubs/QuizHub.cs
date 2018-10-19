1252c93c85010910e8d3d9e97c3828c66e87b211
Wim Van Laer <van.laer.wim@gmail.com>
2018-10-19 20:31:03 +0200

    Removed Lobby hub, created methods for QuizHub

--- QuizMaster.SocketApp/Hubs/QuizHub.cs
+++ QuizMaster.SocketApp/Hubs/QuizHub.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using QuizMaster.Shared.Models;
 
@@ -5,6 +6,20 @@ namespace QuizMaster.SocketApp.Hubs
 {
     public class QuizHub : Hub<User>
     {
-        
+        public async Task JoinQuiz(string lobby, string username, string token)
+        {
+        }
+
+        public async Task LeaveQuiz(string lobby, string username, string token)
+        {
+        }
+
+        public async Task KickFromQuiz(string lobby, string token, string userToKick)
+        {
+        }
+
+        public async Task StartQuiz(string lobby, string token)
+        {
+        }
     }
 }
