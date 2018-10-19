887503dd991bf2c202aac5708b23be5fe964fb10
Wim Van Laer <van.laer.wim@gmail.com>
2018-10-19 22:53:44 +0200

    Gave User ConnectionIds for the sockets

--- QuizMaster.Shared/Models/User.cs
+++ QuizMaster.Shared/Models/User.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
+
 namespace QuizMaster.Shared.Models
 {
     public class User : Base.Model
     {
-        
+        public List<string> ConnectionIds { get; set; }
     }
 }
