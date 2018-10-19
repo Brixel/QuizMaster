a7ec2e0e3cda127b1854a5bc80b8db2b3813af6a
Wim Van Laer <van.laer.wim@gmail.com>
2018-10-19 22:45:52 +0200

    Implemented method for the game hub + implemented IDataService in IGameService

--- QuizMaster.SocketApp.Services/Interfaces/IGameService.cs
+++ QuizMaster.SocketApp.Services/Interfaces/IGameService.cs
@@ -1,8 +1,9 @@
 using System.Threading.Tasks;
+using QuizMaster.Shared.Models;
 
 namespace QuizMaster.SocketApp.Services.Interfaces
 {
-    public interface IGameService
+    public interface IGameService : IDataService<Game>
     {
         Task JoinGame(int game, int user);
         Task LeaveGame(int game, int user);
--- QuizMaster.SocketApp/Hubs/GameHub.cs
+++ QuizMaster.SocketApp/Hubs/GameHub.cs
@@ -7,28 +7,35 @@ namespace QuizMaster.SocketApp.Hubs
 {
     public class GameHub : Hub<User>
     {
-        private readonly IDataService<Game> _gameService;
+        private readonly IGameService _gameService;
 
-        public GameHub(IDataService<Game> gameService)
+        public GameHub(IGameService gameService)
         {
             _gameService = gameService;
         }
-        
+
         public async Task JoinGame(int game, int user, string token)
         {
-            
+            // TODO verify the user with the token
+            await _gameService.JoinGame(game, user);
         }
 
         public async Task LeaveGame(int game, int user, string token)
         {
+            // TODO verify the user with the token
+            await _gameService.LeaveGame(game, user);
         }
 
         public async Task KickFromGame(int game, string token, int userToKick)
         {
+            // TODO verify the user with the token
+            await _gameService.LeaveGame(game, userToKick);
         }
 
         public async Task StartGames(int game, string token)
         {
+            // TODO verify the user with the token
+            await _gameService.StartGame(game);
         }
     }
 }
