672aa2d7107edced01f3f90af360ed1f5d082f33
Berend Wouters <berend.wouters@gmail.com>
2018-10-23 21:53:30 +0200

    Adds IsPublic and Password field to Quiz
    Also adds ctor

--- QuizMaster.Shared/Models/Quiz.cs
+++ QuizMaster.Shared/Models/Quiz.cs
@@ -5,7 +5,14 @@ namespace QuizMaster.Shared.Models
 {
     public class Quiz : Model
     {
+        public Quiz()
+        {
+            Rounds = new List<Round>();
+            IsPublic = true;
+        }
         public string Title { get; set; }
         public IEnumerable<Round> Rounds { get; set; }
+        public bool IsPublic { get; set; }
+        public string Password { get; set; } 
     }
 }
